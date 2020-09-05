b8a09319ef6e170b8617853b672d98c05fa4a7b6
Caleb <calebcoecode@gmail.com>
2020-09-05 14:16:33 -0600

    Added another HTTP request using Mapbox

--- weather-app/app.js
+++ weather-app/app.js
@@ -10,3 +10,11 @@ request({ url: url, json: true }, (err, res) => {
     console.log(chalk.green(res.body.current.weather_descriptions[0] + '. It is currently ' + res.body.current.temperature + 
         ' degrees outside. It feels like ' + res.body.current.feelslike + ' degrees.'))
 });
+
+const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic2ltcGxlY29udHJpYnV0b3IiLCJhIjoiY2tlcTJ1aGQ1MGhkYTJ3dDhncWZwZ21sZCJ9.NRXZMLGR226kVukxtzxJFA';
+
+request({ url: geocodeUrl, json: true }, (err, res) => {
+    const longitude = res.body.features[0].center[0];
+    const latitude = res.body.features[0].center[1];
+    console.log(longitude, latitude);
+});
