148da81764e8cf64e53fb39eb3836027db66cb80
Caleb <calebcoecode@gmail.com>
2020-11-10 20:40:18 -0700

    Add all CRUD operations. Basics of MongoDB.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,19 +13,122 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
 
     const db = client.db(databaseName);
 
-    db.collection('tasks').findOne({ _id: new ObjectID("5f6e240a3f78d41d7c99da99") }, (error, task) => {
-        if(error){
-            return console.log('Unable to fetch')
-        }
+    //////////////////////////
+    ///////  CREATE  /////////
+    //////////////////////////
+    // db.collection('users').insertOne({
+    //        name: 'Katalina',
+    //        age: 22 
+    // }, (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
 
-        console.log(task);
-    })
+    //     console.log(result.ops)
+    // })
 
-    db.collection('tasks').find({ status: false }).toArray((error, tasks) => {
-        if(error){
-            return console.log('Unable to fetch')
-        }
 
-        console.log(tasks);
-    })
+    // db.collection('users').insertMany([
+    //     {
+    //        name: 'Caleb',
+    //        age: 26 
+    //     },
+    //     {
+    //         name: 'Katalina',
+    //         age: 22
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+    //////////////////////////
+    //////////////////////////
+
+
+
+
+
+    //////////////////////////
+    ////////  READ  //////////
+    //////////////////////////
+    // db.collection('tasks').findOne({ _id: new ObjectID("5f6e240a3f78d41d7c99da99") }, (error, task) => {
+    //     if(error){
+    //         return console.log('Unable to fetch')
+    //     }
+
+    //     console.log(task);
+    // })
+
+
+    // db.collection('tasks').find({ status: false }).toArray((error, tasks) => {
+    //     if(error){
+    //         return console.log('Unable to fetch')
+    //     }
+
+    //     console.log(tasks);
+    // })
+    //////////////////////////
+    //////////////////////////
+
+
+
+
+    //////////////////////////
+    ///////  UPDATE  /////////
+    //////////////////////////
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("5f6e22a6b354ec3628aeb70b")
+    // }, {
+    //     $set: {
+    //         name: 'Like Mike'
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+
+    // db.collection('tasks').updateMany({
+    //     status: true
+    // }, {
+    //     $set: {
+    //         status: false
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    //////////////////////////
+    //////////////////////////
+
+
+
+
+    //////////////////////////
+    ///////  DELETE  /////////
+    //////////////////////////
+    // db.collection('tasks').deleteOne({
+    //     task: "Go to work"
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    
+    // db.collection('users').deleteMany({
+    //     age: 30
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+    //////////////////////////
+    //////////////////////////
 })
