78ed7290280fcd0420ef2fe3b225052b11bcd726
Caleb <calebcoecode@gmail.com>
2020-11-15 17:15:53 -0700

    Learn about async/await and incorporate it into index.js in place of promises

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5faef05703dd010c84a7625c').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ status: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5faef05703dd010c84a7625c').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ status: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+const deleteTaskAndCount = async (id, status) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ status })
+    return count
+}
+
+deleteTaskAndCount('5fb18aedb3a3122fb81c7b0c', false).then((count) => {
+    console.log(count)
 }).catch((err) => {
     console.log(err)
 })
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -3,11 +3,23 @@ const User = require('../src/models/user');
 
 //5faecc0aeb6006249c035903
 
-User.findByIdAndUpdate('5fac263666e1ef4888d87e5c', { age: 26 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5fac263666e1ef4888d87e5c', { age: 26 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5fac263666e1ef4888d87e5c', 2).then((count) => {
+    console.log(count)
 }).catch((err) => {
     console.log(err)
 })
