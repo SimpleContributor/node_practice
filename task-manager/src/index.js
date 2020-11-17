629075d9fb2b85f1c707151ab3ea2dd095b3947c
Caleb <calebcoecode@gmail.com>
2020-11-16 21:13:31 -0700

    Learn about password encryption with the npm package bcryptjs. Incorporate into the user model for login

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,127 +1,33 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-
-    try{
-        await user.save()
-        res.status(201).send(user)
-    } catch(err) {
-        res.status(400).send(err)
-    }
-
-    // user.save().then(() => {
-    //     res.status(201).send(user)
-    // }).catch((error) => {
-    //     res.status(400).send(error)
-    // })
-});
-
-app.get('/users', async (req, res) => {
-    try {
-       const users = await User.find({})
-       res.status(201).send(users)
-    } catch(err) {
-        res.status(400).send(err)
-    }
-
-    // User.find({}).then((users) => {
-    //     res.send(users)
-    // }).catch((error) => {
-    //     res.status(500).send()
-    // })
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch(err) {
-        res.status(500).send(err)
-    }
-
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(user)
-    // }).catch((error) => {
-    //     res.status(500).send()
-    // })
-})
-
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
-
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch(err) {
-        res.status(400).send(err)
-    }
-
-    // task.save().then(() => {
-    //     res.status(201).send(task);
-    // }).catch((error) => {
-    //     res.status(400).send(error)
-    // })
-})
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const task = await Task.find({})
-        res.status(200).send(task)
-    } catch(err) {
-        res.status(500).send(err)
-    }
-
-    // Task.find({}).then((tasks) => {
-    //     res.send(tasks)
-    // }).catch((error) => {
-    //     res.status(500).send()
-    // })
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const task = await Task.findById(_id)
-        if(!task) {
-            res.status(404).send()
-        }
-
-        res.status(200).send(task)
-    } catch(err) {
-        res.status(500).send(err)
-    }
-
-    // Task.findById(_id).then((task) => {
-    //     if (!task) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(task)
-    // }).catch((error) => {
-    //     res.status(500).send()
-    // })
-})
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
+
+
+
+// const bcrypt = require('bcryptjs');
+
+// const myFunction = async () => {
+//     const password = 'Red12345!';
+
+//     const hashedpassword = await bcrypt.hash(password, 8);
+
+//     console.log(password);
+//     console.log(hashedpassword);
+
+//     const isMatch = await bcrypt.compare('Red12345', hashedpassword);
+//     console.log(isMatch);
+// }
+
+// myFunction();
---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const Task = require('../models/task');
+const router = new express.Router();
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const task = await Task.find({})
+        res.status(200).send(task)
+    } catch(err) {
+        res.status(500).send(err)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(err) {
+        res.status(500).send(err)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['task', 'status'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    const _id = req.params.id;
+
+    if(!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findById(_id);
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save();
+
+        // const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true });
+
+
+        if(!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if(!task) {
+            res.status(404).send();
+        }
+
+        res.send(task);
+    } catch(err) {
+        res.status(500).send(err);
+    }
+})
+
+module.exports = router;
