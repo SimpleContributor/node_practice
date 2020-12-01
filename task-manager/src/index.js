ea0dc6fa54240e7913fc745bb04357d3ebefeba4
Caleb <calebcoecode@gmail.com>
2020-12-01 13:38:34 -0700

    Add routes for users to post, delete and get avatar image to and from the database. Add npm packages multer and sharp.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,33 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-const multer = require('multer');
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000 // 1MB megabyte
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word Document'))
-        }
-
-        cb(undefined, true)
-
-        // cb(new Error('File must be a PDF'))
-        // cb(undefined, true)
-    }
-});
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
-
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -40,5 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
-
-
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -49,7 +49,11 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }],
+
+    avatar: {
+        type: Buffer,
+    }
 }, {
     timestamps: true,
 })
@@ -70,6 +74,7 @@ userSchema.methods.toJSON = function() {
 
     delete userObject.password;
     delete userObject.tokens;
+    delete userObject.avatar;
 
     return userObject;
 }
