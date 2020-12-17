83f5eb87d390760ff2592123735d220f4b92d77b
Caleb <calebcoecode@gmail.com>
2020-12-16 20:15:17 -0700

    Add more task route tests for get and delete

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,8 +1,15 @@
-const { response } = require('express');
 const request = require('supertest');
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
+const { 
+    userOneId, 
+    userOne, 
+    userTwo, 
+    setupDatabase, 
+    taskOne, 
+    taskTwo, 
+    taskThree 
+} = require('./fixtures/db')
 
 beforeEach(setupDatabase);
 
@@ -19,3 +26,23 @@ test('Should create task for user', async () => {
     expect(task).not.toBeNull();
     expect(task.status).toEqual(false)
 });
+
+test('Should get userOne tasks', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(200)
+
+    expect(response.body.length).toEqual(2)
+})
+
+test('Should NOT delete different users tasks', async () => {
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+
+    const task = await Task.findById(taskOne._id);
+    expect(task).not.toBeNull();
+})
